3029185d9d0266cf4814177f1e4a78a08b3bc798
Blackdarkbro <egorsavchik98@mail.ru>
2019-01-27 20:43:11 +0300

    finally

--- mainJS.js
+++ mainJS.js
@@ -1,4 +1,6 @@
-            var logo = document.getElementsByClassName('head-logo')[0];
+
+// ссылка на главное меню
+var logo = document.getElementsByClassName('head-logo')[0];
             $('.head-logo').click(function(){
                 window.location = "../index.html"
             })
@@ -6,8 +8,9 @@
                 logo.style.cursor = "pointer";
             })
 
-    jQuery(document).ready(function(){
-  jQuery('.bxSlider').bxSlider({
+// Подключение и настройка слайдера
+    $(document).ready(function(){
+  $('.bxSlider').bxSlider({
       hideControlOnEnd: true,
       mode: 'vertical',
       speed: 1200,
@@ -19,3 +22,19 @@
   });
     });
       
+// Подключение XML
+if (window.XMLHttpRequest)
+  { // для IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp = new XMLHttpRequest();}
+  else
+  { // для IE6, IE5
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");}
+    xmlhttp.open("GET","xml/cart1.xml",false);
+    xmlhttp.send();
+    xmlDoc = xmlhttp.responseXML;
+var name = document.getElementsByClassName("name");
+for(var i = 0; i < name.length; i++ ){
+    document.getElementsByClassName("name")[i].innerHTML = xmlDoc.getElementsByTagName("name")[i].childNodes[0].nodeValue;
+    document.getElementsByClassName("characteristic")[i].innerHTML = xmlDoc.getElementsByTagName("characteristic")[i].childNodes[0].nodeValue;
+    document.getElementsByClassName("cena")[i].innerHTML = xmlDoc.getElementsByTagName("price")[i].childNodes[0].nodeValue;
+}
